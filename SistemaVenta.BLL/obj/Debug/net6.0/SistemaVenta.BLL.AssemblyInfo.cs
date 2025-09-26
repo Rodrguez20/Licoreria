65ba79ebbb5bd4fec5afa4474bdbf8c85ad5d89b
Calixto Rodriguez <dianavdonaire@gmail.com>
2025-09-26 14:28:08 -0600

    actualizacion de las vistas

--- SistemaVenta.BLL/obj/Debug/net6.0/SistemaVenta.BLL.AssemblyInfo.cs
+++ SistemaVenta.BLL/obj/Debug/net6.0/SistemaVenta.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaVenta.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bc797f0f48ecef004b8ae291c0e1f279ef78611")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaVenta.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaVenta.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
