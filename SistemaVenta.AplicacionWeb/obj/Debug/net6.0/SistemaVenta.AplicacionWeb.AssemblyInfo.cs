354605fe6dd4b1e232688b9e0cd28f433ffaeafe
Calixto Rodriguez <dianavdonaire@gmail.com>
2025-09-26 18:15:14 -0600

    actua

--- SistemaVenta.AplicacionWeb/obj/Debug/net6.0/SistemaVenta.AplicacionWeb.AssemblyInfo.cs
+++ SistemaVenta.AplicacionWeb/obj/Debug/net6.0/SistemaVenta.AplicacionWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bc797f0f48ecef004b8ae291c0e1f279ef78611")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65ba79ebbb5bd4fec5afa4474bdbf8c85ad5d89b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaVenta.AplicacionWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
